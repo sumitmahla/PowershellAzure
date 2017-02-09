665c82ef9f46a6dbe2cd81dea8291d2d78db05d5
Sumit Mahla <sumitmahla@hotmail.com>
2017-02-09 12:38:39 +1100

    Create RDPICMPEnable.ps1

---
+++ RDPICMPEnable.ps1
@@ -0,0 +1,13 @@
+[DscLocalConfigurationManager()]
+Configuration RDPICMPEnable
+{
+    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
+
+    netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol="icmpv4:8,any" dir=in action=allow
+
+
+}
